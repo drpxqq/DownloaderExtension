fdc19881280554081c98440e64a84196711c0c3d
drpxqq <141672412+drpxqq@users.noreply.github.com>
2026-07-31 02:04:35 +0300

    Create downloader.user.js

---
+++ downloader.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Downloader
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Downloader
+// @author       downsyndrome
+// @match        https://vk.ru/*
+// @icon         https://vk.ru/images/icons/favicons/fav_logo.ico
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
